9866f5d0170e265fa3bd453a0d388a0cf07ac7cc
Nicola Toscan <nicolatoscan99@outlook.it>
2021-06-02 04:42:28 +0200

    Recycler View

--- app/src/main/java/it/rememo/rememo/ui/collections/CollectionsFragment.java
+++ app/src/main/java/it/rememo/rememo/ui/collections/CollectionsFragment.java
@@ -9,7 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayoutMediator;
 
--- app/src/main/java/it/rememo/rememo/ui/collections/CollectionsGroupFragment.java
+++ app/src/main/java/it/rememo/rememo/ui/collections/CollectionsGroupFragment.java
@@ -5,10 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import it.rememo.rememo.R;
 
@@ -16,6 +21,7 @@ import it.rememo.rememo.R;
 // object in our collection.
 public class CollectionsGroupFragment extends Fragment {
     public static final String ARG_OBJECT = "object";
+    CollectionRecyclerViewAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +31,29 @@ public class CollectionsGroupFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Bundle args = getArguments();
-        ((TextView) view.findViewById(R.id.text1)).setText(Integer.toString(args.getInt(ARG_OBJECT)));
+
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+        animalNames.add("Pippo");
+        animalNames.add("Pluto");
+        animalNames.add("Paperino");
+        animalNames.add("Ornitorinco");
+        animalNames.add("Anatra");
+        animalNames.add("Pinguino");
+        animalNames.add("Panda");
+        animalNames.add("Koala");
+
+        RecyclerView recyclerView = view.findViewById(R.id.collectionRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new CollectionRecyclerViewAdapter(getContext(), animalNames);
+        adapter.setClickListener((v, position) -> {
+            Toast.makeText(getContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+        });
+        recyclerView.setAdapter(adapter);
+
     }
 }
